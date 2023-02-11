331915cddd6ee45764a194fc70fd235b14a421dc
rushimisha dieudonne <rushimisha95@ggmail.com>
2023-02-11 17:35:32 +0200

    CircleCI configaration

--- src/__tests__/blogs.test.js
+++ src/__tests__/blogs.test.js
@@ -38,19 +38,23 @@ describe("testing if database is connected", () => {
   test("GET single blog", async () => {
     const blog_id = "63e4e79d86bdba507f13c87f";
     //get the result expecting success and JSON data
-    const resItem = await request(app).get(`/blogs/${blog_id}`).expect(200);
+    const resItem = await request(app)
+      .get(`/api/v1/blogs/${blog_id}`)
+      .expect(204);
   });
 
   test("delete single blog", async () => {
     const blog_id = "63e4e79d86bdba507f13c87f";
     //get the result expecting success and JSON data
-    const resItem = await request(app).delete(`/blogs/${blog_id}`).expect(200);
+    const resItem = await request(app)
+      .delete(`/api/v1/blogs/${blog_id}`)
+      .expect(204);
   });
   test("comment on single blog", async () => {
     const blog_id = "63e64fcded0536b9af6dc551";
     //get the result expecting success and JSON data
     const resItem = await request(app)
-      .post(`api/v1/blogs/${blog_id}/comments`)
+      .post(`/api/v1/blogs/${blog_id}/comments`)
       .send(comment)
       .expect(200);
   });
